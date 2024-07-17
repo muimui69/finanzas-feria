91cd01eaa289bbe2c333371c95776f99741bf2e3
muimui69 <moises_c2015@outlook.es>
2024-07-17 08:36:38 -0400

    .

--- src/pages/intereses/simple.tsx
+++ src/pages/intereses/simple.tsx
@@ -3,19 +3,11 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
 import { calcularInteresSimple } from "@/math/intereses"
 import { FormEvent, useState } from "react"
 
